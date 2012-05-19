c19cd48c305238d8b351891eb9fafde9784d1f8f
Irio Irineu Musskopf Junior <irio@startocean.com>
2012-05-19 08:39:49 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AssetPipeline::Application.config.secret_token = '158c17168ed6d85a0580fbbf08049e2f24ce04bd611eda100d682788d9492b0ae9cc2748e5d3cc21542939206d54b225b9d7d9f95fa197ace8e28f21fbb3e38d'
